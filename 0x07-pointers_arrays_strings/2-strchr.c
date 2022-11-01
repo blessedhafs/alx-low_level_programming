c308e8b7b763ddf7b2dfa942fb41c87d4c76aca7
blessedhafs <hafsohajani23@gmail.com>
2022-10-31 18:44:46 -0700

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,12 +11,12 @@ char *_strchr(char *s, char c)
 	int a = 0, b;
 
 	while (s[a])
-		a++;
+        	a++;
 	for (b = 0; b < a; b++)
 	{
 		if (c == s[b])
-			s += b;
-			return (s);
+		    s += b;
+		    return (s);
 	}
 	return ('\0');
 }
