17efa8884b0de6d67a9623a0717454c4e8f12ad5
blessedhafs <hafsohajani23@gmail.com>
2022-11-05 09:41:20 -0700

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * main -> print the name of the program
+ * main - print the name of the program
  * @argc: count arguments
  * @argv: Arguments
  *
@@ -12,7 +12,7 @@
 int main(int argc, char *argv[])
 {
 	(void) argc;
-	printf("%s\n", *argv[0]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
